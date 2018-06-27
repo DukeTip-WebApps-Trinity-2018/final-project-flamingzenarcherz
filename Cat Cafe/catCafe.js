6f9eea310878f05ab2de11f098cc244184843a3d
EmptyBowl320 <40175855+EmptyBowl320@users.noreply.github.com>
2018-06-27 15:47:28 -0500

    Add files via upload

--- Cat Cafe/catCafe.js
+++ Cat Cafe/catCafe.js
@@ -1,26 +1,24 @@
-window.onload = function() {
-$(".submit").click(function() {
+window.onload = function () {
+  $(".submit").click(function () {
     var city = $(".city").val()
     var uri = "https://api.openweathermap.org/data/2.5/weather?zip=" + city + ",us&units=imperial&appid=11ea12a822a12cbc1722c219d2adb648";
     $.get(uri, onRes);
-});
- 
+  });
+
 }
 
 function onRes(response) {
-    $(".country").html(response.sys.country);
-    $(".temp").html(response.main.temp);
-    $(".city-name").html(response.name);
-    $(".weather").html(response.weather[0].description);
-}  
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  $(".country").html(response.sys.country);
+  $(".temp").html(response.main.temp);
+  $(".city-name").html(response.name);
+  $(".weather").html(response.weather[0].description);
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
 
     var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -33,3 +31,13 @@ window.onclick = function(event) {
     }
   }
 }
+
+var password = prompt("Please Enter the Password");
+
+if (password == null || password != "#7jnsuot7C9947HDJhOS0f*78asdf2354542335132154987&&&*785878^%()9389989y7LJDJSU9&^TVU&E&YVuhc|FP-_R||*$))@__7&X&C*V(ISD&t0$))*7") {
+  txt = "Password Incorrect.";
+} else {
+  txt = "Welcome to the secret restaurant! What would you like to order?";
+}
+
+window.alert(txt);
