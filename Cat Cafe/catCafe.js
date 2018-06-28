a8701d3b5ba556c631d66133cad475d31c539749
EmptyBowl320 <40175855+EmptyBowl320@users.noreply.github.com>
2018-06-28 14:08:00 -0500

    Add files via upload

--- Cat Cafe/catCafe.js
+++ Cat Cafe/catCafe.js
@@ -4,7 +4,6 @@ window.onload = function () {
     var uri = "https://api.openweathermap.org/data/2.5/weather?zip=" + city + ",us&units=imperial&appid=11ea12a822a12cbc1722c219d2adb648";
     $.get(uri, onRes);
   });
-
 }
 
 function onRes(response) {
@@ -12,32 +11,34 @@ function onRes(response) {
   $(".temp").html(response.main.temp);
   $(".city-name").html(response.name);
   $(".weather").html(response.weather[0].description);
+  recomendations(response);
 }
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+function recomendations(response) {
+  if (response.main.temp > 200) {
+    $(".recommend").html("U is already dead");
+  }
+  if (response.main.temp > 100 && response.main.temp <= 200) {
+    $(".recommend").html("It's really hot; We recommend iced coffee and some profiteroles.");
+    $('#recommendImage').prepend('<img id="image" src="https://weather.sumofus.org/static/weather-images/carousel/sun.png" />')
+  }
+  if (response.main.temp > 85 && response.main.temp <= 100) {
+    $(".recommend").html("It's quite hot; We recommend boba tea and a classic turkey sandwich.");
+    $('#recommendImage').prepend('<img id="image" src="https://weather.sumofus.org/static/weather-images/carousel/sun.png" />')
+  }
+  if (response.main.temp > 60 && response.main.temp <= 85) {
+    $(".recommend").html("It's a nice temperature; We recommend a Sprite and a muffin.");
+      $('#recommendImage').prepend('<img id="image" src="https://weather.sumofus.org/static/weather-images/carousel/sun.png" />')
+  }
+  if (response.main.temp > 30 && response.main.temp <= 60) {
+    $(".recommend").html("It's pretty chilly; We recommend a hot latte and a caesar salad.");
+  }
+  if (response.main.temp <= 30 && response.main.temp > 55) {
+    $(".recommend").html("It's cold out; We recommend a steaming espresso and a hot churro.");
+  }
+  if (response.main.temp <= 5) {
+    $(".recommend").html("Do you really want to go outside?");
+  }
+  if (response.weather.main == "Scattered Clouds") {
+    $(".recommend").html("It's raining");
   }
 }
-
-var password = prompt("Please Enter the Password");
-
-if (password == null || password != "#7jnsuot7C9947HDJhOS0f*78asdf2354542335132154987&&&*785878^%()9389989y7LJDJSU9&^TVU&E&YVuhc|FP-_R||*$))@__7&X&C*V(ISD&t0$))*7") {
-  txt = "Password Incorrect.";
-} else {
-  txt = "Welcome to the secret restaurant! What would you like to order?";
-}
-
-window.alert(txt);
